61d3e6e20f3825b2b50b38c88c316dcd75f02ea8
KhuloodGhazi <khulood.gmo@gmail.com>
2024-11-03 22:33:57 +0300

    I DID IT, ITS WORKED

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
+from.models import Blog
+
 # Create your views here.
 
 def main_view(request:HttpRequest):
 
-    return render(request, "main/home.html")
+    if request.method == "POST":
+        new_post = Blog(title=request.POST["title"], main_photo=request.POST["main_photo"], content=request.POST["content"])
+        new_post.save()
+    else:
+        print("Error")
+        
+    posts = Blog.objects.all()   
+
+    return render(request, "main/home.html", {"posts" : posts})
 
 
 
