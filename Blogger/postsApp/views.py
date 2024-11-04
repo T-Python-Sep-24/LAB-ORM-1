174ba7ac8ddc855288f0219ab72a0ca290b63868
HmzhDubh <meezo.2015@hotmail.com>
2024-11-04 14:19:43 +0300

    redirect user to main after adding img

--- Blogger/postsApp/views.py
+++ Blogger/postsApp/views.py
@@ -4,11 +4,14 @@ from .models import Post
 # Create your views here.
 
 def create_post_view(request: HttpRequest):
-    print(request.POST)
+    # print(request.POST)
+
     if request.method == "POST":
         new_post = Post(title=request.POST['title'], content=request.POST['content'])
         new_post.save()
-        redirect('bloggerApp:main_view')
+
+        request = redirect('bloggerApp:main_view')
+        return request
 
     request = render(request, 'create_post.html')
     return request
