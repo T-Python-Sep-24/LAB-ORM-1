5d0e42bdfc00ac7bd521b56c471395608ea4376a
Ghaliah <ghalia.banouna@gmail.com>
2024-11-04 22:23:36 +0300

    Added 500 and 404 error pages

--- Blogger/Blogger/settings.py
+++ Blogger/Blogger/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jks0&ts!n^=g6w(@ms@-mtoa1=*+zmt8vfc_=618ioz=+g!e_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
