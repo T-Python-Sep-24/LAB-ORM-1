36a0a2816286679a59aeff4500a3fa8d93c1d0ab
Atiq <abdullah.s.alatiq@gmail.com>
2024-11-05 01:20:22 +0300

    update and delete

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/',views.add_post , name='add_post'),
+    path('detail/<blog_id>/',views.blog_detail_view , name='blog_detail_view'),
+    path('update/<blog_id>/', views.blog_update_view, name ='blog_update_view'),
+    path('delete/<blog_id>/', views.blog_delete_view, name ='blog_delete_view'),
+
+
 ]
+
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -21,9 +21,36 @@ def add_post(request: HttpRequest):
             content=request.POST["content"],
             is_published=True if request.POST.get("is_published") == "on" else False,
             published_at=request.POST.get("published_at", timezone.now()),
+            poster = request.FILES("poster"),
         )
         new_post.save()
         return redirect('home')
 
     return render(request, "blog/add_post.html" )
+
+
+def blog_detail_view(request: HttpRequest, blog_id:int):
+    post = Post.objects.get(pk=blog_id)
+    return render(request, "blog/detail.html", {'post': post})
+
+def blog_update_view(request: HttpRequest, blog_id:int):
+    post = Post.objects.get(pk=blog_id)
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        post.is_published = True if request.POST.get("is_published") == "on" else False
+        post.published_at = request.POST.get("published_at", timezone.now())
+        if "poster" in request.FILES: post.poster=request.FILES["poster"]
+        post.save()
+        return redirect("blog:blog_detail_view", blog_id=post.id)
+    
+
+    return render(request, "blog/update.html", {'post': post})
+
+
+def blog_delete_view(request: HttpRequest, blog_id:int):
+    post = Post.objects.get(pk=blog_id)
+    post.delete()
+    return redirect('blog:home')
+
 
