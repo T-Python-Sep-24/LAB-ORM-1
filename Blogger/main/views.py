239c9373bf066bf5963e18d75fe7d29ec1cee6dd
AR <aarokan@gmail.com>
2024-11-05 03:38:41 +0300

    add image feature

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import Post
 
@@ -11,7 +11,9 @@ def home_view(request: HttpRequest):
 
 def add_view(request: HttpRequest):
     if request.method == "POST":
-        post = Post(title = request.POST["title"], content = request.POST["content"], is_published = bool(request.POST.get("is_published", False)))
+        post = Post(title = request.POST["title"], content = request.POST["content"], is_published = bool(request.POST.get("is_published", False)), image = request.FILES["image"])
         post.save()
+
+        return redirect("main:home_view")
 
     return render(request, "main/add.html")
