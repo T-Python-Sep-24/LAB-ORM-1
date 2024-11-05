a781914f56fcaaf0fbe702719c2cfa0776a04854
Eyad <eyalghamdi@outlook.com>
2024-11-05 12:36:43 +0300

    Update ORM Lab

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -11,21 +11,24 @@ def home_view(request:HttpRequest):
 
 def create_view(request:HttpRequest):    
     if request.method == "POST":
-        new_post = Post(title=request.POST["title"],content=request.POST["description"])
+        new_post = Post(title=request.POST["title"],content=request.POST["description"],category= request.POST["category"])
         if "image" in request.FILES: new_post.image = request.FILES["image"]
         new_post.save()
     return render(request, "main/create.html")
 
 def detail_view(request:HttpRequest,post_id:int):
-    post_detail = Post.objects.get(pk=post_id)
-    return render(request,"main/detail.html", {"post_detail":post_detail})
+    try:
+        post_detail = Post.objects.get(pk=post_id)
+        return render(request,"main/detail.html", {"post_detail":post_detail})
+    except Exception as e :
+        return render(request,"main/page_not_found.html")
 
 def update_view(request:HttpRequest,post_id:int):
     post_detail = Post.objects.get(pk=post_id)
-
     if request.method == "POST":
         post_detail.title = request.POST["title"]
         post_detail.content = request.POST["description"]
+        post_detail.category = request.POST["category"]
         if "image" in request.FILES: post_detail.image = request.FILES["image"]
         post_detail.save()
 
