55560a1350c5968572b7cf632dbdbac9f7e4532a
AR <aarokan@gmail.com>
2024-11-05 06:04:27 +0300

    add update and delete feaatures

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -6,4 +6,8 @@ app_name = "main"
 urlpatterns = [
     path("", views.home_view, name = "home_view"),
     path("add/", views.add_view, name = "add_view"),
+    path("details/<post_id>/", views.post_details_view, name = "post_details_view"),
+    path("notfound/", views.not_found_view, name = "not_found_view"),
+    path("update/<post_id>/", views.update_view, name = "update_view"),
+    path("delete/<post_id>/", views.delete_view, name = "delete_view" ),
 ]
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -17,3 +17,55 @@ def add_view(request: HttpRequest):
         return redirect("main:home_view")
 
     return render(request, "main/add.html")
+
+
+def post_details_view(request: HttpRequest, post_id: int):
+    try:
+        post = Post.objects.get(pk = post_id)
+    except:
+        return redirect("main:not_found_view")
+
+    return render(request, "main/details.html", {"post": post})
+
+
+def not_found_view(request: HttpRequest):
+
+    return render(request, "main/notfound.html")
+
+
+def update_view(request: HttpRequest, post_id):
+    try:
+        # get the post by the id
+        post = Post.objects.get(pk = post_id)
+
+        # Check if the form was submited by post method
+        if request.method == "POST":
+            post.title = request.POST["title"]
+            post.content = request.POST["content"]
+            post.is_published = request.POST["is_published"]
+            # check if he updated the image
+            if "image" in request.FILES:
+                post.image = request.FILES["image"]
+            post.save()
+
+            # redirect to details pass the id as path param details/id/
+            return redirect("main:post_details_view", post_id = post.id)
+
+    except:
+        return redirect("main:not_found_view")
+
+    return render(request, "main/update.html", {"post": post})
+
+
+def delete_view(request: HttpRequest, post_id: int):
+    try:
+        # get the post by the id
+        post = Post.objects.get(pk = post_id)
+
+        post.delete()
+    except:
+        return redirect("main:not_found_view")
+
+    return redirect("main:home_view")
+
+    
